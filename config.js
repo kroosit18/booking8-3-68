07169b2aac297b8623198f423b4d39b12be2b654
kroosit18 <kroosit118@gmail.com>
2025-03-08 20:30:48 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 export default {
-    aprUrl: "https://script.google.com/macros/s/AKfycbxWVh6dahxCH4KCZThHsN3h9AMLyFrch7BhsxPQl6tnWR_JUcORM7QAA5uRNgqs7fyfAg/exec"
+    aprUrl: "https://script.google.com/macros/s/AKfycbxVUinvWzsp178oiD7ldBJFlc3J-Bke1377PsJ9uuBIbrKgI-TlxuO0xtzmqSV3af0b/exec"
 }
